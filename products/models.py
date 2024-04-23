797c0c553718c513e829747ca1505bc074423ed2
Gulam jilani <ghulamjilanideveloper@gmail.com>
2024-04-23 18:39:37 +0500

    add data in the database

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator
 
 class Products(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=255)
     unit_price = models.DecimalField(
         max_digits=6, 
         decimal_places=2,
@@ -14,9 +14,9 @@ class Products(models.Model):
     inventory = models.IntegerField(
         validators=[MinValueValidator(1)]
     )
-    image = models.ImageField(upload_to='product/')
+    image = models.ImageField(upload_to='product/',max_length=255)
     description = models.TextField()
-    last_update = models.DateTimeField(auto_now=True)
+    last_update = models.DateField(auto_now=True)
  
     def __str__(self):
         return self.title
@@ -36,7 +36,3 @@ class Order(models.Model):
     price = models.IntegerField()
     quantity = models.PositiveSmallIntegerField()
     placed_at = models.DateTimeField(auto_now_add=True)
-
-    
-
-        
