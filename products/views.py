9d957d54f938dbd5a73e8230a46993421e1f9b02
unknown <arslan.ahmad@tiksom.com>
2024-04-28 21:23:32 +0500

    Latest recommendation model integrated successfully

--- products/views.py
+++ products/views.py
@@ -1,47 +1,23 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
 from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets, permissions
 from .serializers import *
 from .models import *
 
-
-from django.http import JsonResponse
-import pickle
-import numpy as np
-import pandas as pd
+from django.http import JsonResponse, HttpResponse
 from sklearn.preprocessing import LabelEncoder
 from gensim.models import Word2Vec
-from django.shortcuts import render
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django.db.models import Sum
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes,api_view
 
-
-from sklearn.decomposition import PCA
-from django.db.models import Q
-from django.shortcuts import render
-from .models import Order, Products
-from django.http import JsonResponse,HttpResponse 
-from django.views.decorators.csrf import csrf_exempt
-import json
-import joblib
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing import image as img_preprocessing
+
 import numpy as np
+import pandas as pd
+import pickle
 import requests
-from PIL import Image
-import io
-from Eshop import settings
-import requests
-from django.http import HttpResponse
-from rest_framework import generics
-import requests
-from django.core.paginator import Paginator
-from rest_framework.pagination import PageNumberPagination
 
-
-# Create your views here.
 
 def IndexPageView(request):
     return render(request, "index.html")
@@ -95,7 +71,7 @@ class OrderCreation(generics.CreateAPIView):
 
 class UserHistoryCreate(generics.CreateAPIView):
     
-    serializer_class = OrderWriteSerializer
+    serializer_class = UserHistorySerializer
     queryset = UserHistory.objects.all()
 
 
@@ -142,7 +118,6 @@ def average_word_vectors(words, model, vocabulary, num_features):
 
 def predict_price_view(request):
     if request.method == 'POST':
-        # Load the model
         with open('./model/product_price_predictions_model.pkl', 'rb') as f:
             model = pickle.load(f)
         
@@ -193,90 +168,6 @@ def predict_price_view(request):
         # Render the HTML form
         return render(request, 'priceprediction.html')
 
-# Load the KNN model
-knn_model = joblib.load('./model/knn_model.pkl')
-
-# Load the VGG16 base model
-base_model = VGG16(include_top=False, input_shape=(256, 256, 3))
-
-def preprocess_image(image):
-    # Resize and preprocess the image
-    image = image.resize((256, 256))
-    image = img_preprocessing.img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = preprocess_input(image)
-    return image
-
-def load_image_from_url(url):
-    response = requests.get(url)
-    image = Image.open(io.BytesIO(response.content))
-    return image
-
-def extract_features(image):
-    # Preprocess the image
-    preprocessed_image = preprocess_image(image)
-    # Extract features using the VGG16 base model
-    features = base_model.predict(preprocessed_image)
-    return features
-
-# @api_view(['GET'])
-# # @permission_classes((IsAuthenticated, ))
-# def recommend_products(request, user_id):
-#     if request.method == 'GET':
-#         try:
-#             order = Order.objects.filter(user=user_id).last()
-#         except Order.DoesNotExist:
-#             return JsonResponse({'error': 'Order not found'}, status=404)
-
-#         previous_orders = Order.objects.filter(user=user_id).latest()
-#         user = request.user
-#         print("User is---",user)
-#         # Extract features from images of previous orders
-#         features = []
-#         for previous_order in previous_orders:
-#             imageUrl = ''
-#             url = previous_order.product.image.name
-#             print("Image URL OF ", url)
-#             if "http://assets.myntassets.com" in url:
-#                 print("image assest found")
-#                 imageUrl = url
-#             else:
-#                 imageUrl = settings.SITE_URL + url
-#             image = load_image_from_url(imageUrl)
-#             # Extract features from the image using the VGG16 base model
-#             image_features = extract_features(image)
-#             features.append(image_features)
-
-#         combined_features = np.concatenate(features, axis=0)
-#         num_samples = combined_features.shape[0]
-#         print("num samples---", num_samples)
-#         flattened_features = combined_features.reshape(num_samples, -1)
-        
-#         num_components = min(313, min(combined_features.shape))
-#         pca = PCA(n_components=num_components)
-#         reduced_features = pca.fit_transform(flattened_features)
-        
-#         if reduced_features.shape[1] < 313:
-#             padding = np.zeros((num_samples, 313 - reduced_features.shape[1]))
-#             reduced_features = np.hstack((reduced_features, padding))
-#         print("Befire model prediction")
-#         # similar_product_ids = knn_model.predict(reduced_features)
-#         num_neighbors = 3
-#         distances, indices = knn_model.kneighbors(reduced_features, n_neighbors=num_neighbors)
-#         similar_product_ids = indices.flatten()
-#         print("ID predicted",similar_product_ids)
-#         products = Products.objects.filter(id__in=similar_product_ids)
-#         serializer = ProductReadSerializer(products, many=True)
-#         print("Before serializer------")
-#         return Response(serializer.data)
-    
-#     elif request.method == 'POST':
-#         data = request.POST
-#         return JsonResponse({'received_data': data})
-
-#     else:
-#         # Handle other HTTP methods
-#         return JsonResponse({'error': 'Method not allowed'}, status=405)
        
 @api_view(['GET'])
 def image_proxy(request):
@@ -295,177 +186,49 @@ def image_proxy(request):
         return HttpResponse(status=response.status_code)
 
 
+loaded_model = pickle.load(open('./model/knn_model_features_base.pkl', 'rb'))
+loaded_encoders = pickle.load(open('./model/encoders_features_base.pkl', 'rb'))
+product_predict = pickle.load(open('./model/Y_train.pkl', 'rb')) 
 
+features = ['gender', 'masterCategory', 'subCategory', 'articleType', 'season']
 
-import pickle
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Products
-
-# Load your model and encoders 
-# loaded_model = pickle.load(open('./model/knn_model_features_base.pkl', 'rb'))
-# loaded_encoders = pickle.load(open('./model/encoders_features_base.pkl', 'rb'))
-
-# features = ['gender', 'masterCategory', 'subCategory', 'articleType', 'season']
-
-# def Recommend_product(request,user_id):
-#     previous_orders = Order.objects.filter(user=user_id).last()
-#     print("Last order----", previous_orders.product.id)
-#     print("Last order----", previous_orders.product.gender)
-#     print("Last order----", previous_orders.product.title)
-#     product = previous_orders.product
-#     new_data = {  # Replace with your actual new data point
-#         'gender': product.gender,
-#         'masterCategory': product.mastercategory,
-#         'subCategory': product.subcategory,
-#         'articleType': product.articletype,
-#         'season': product.season
-#     }
-#     print("New data---", new_data)
-#     # data = request.data
-#     new_df = pd.DataFrame([new_data])
-#     print("Dataframe---", new_df)
-#     for feature in features:
-#         new_df[feature] = loaded_encoders[feature].transform(new_df[feature])
-
-#         # Predict and filter IDs
-#         distances, indices = loaded_model.kneighbors(new_df, n_neighbors=3)
-#         valid_ids = []
-#         for i in range(len(indices[0])):
-#             neighbor_index = indices[0][i]
-#             # Get neighbor ID from the database
-#             neighbor_id = Products.objects.all()[neighbor_index].id  
-
-#             # Check if ID is valid
-#             if Products.objects.filter(id=neighbor_id).exists():
-#                 valid_ids.append(neighbor_id)
-
-#         # Return predicted IDs as JSON response
-#     print("valid id's ", valid_ids)
-#     return HttpResponse(request,{"predicted_ids":previous_orders})
-
-
-
-
-
-# views.py
-import pandas as pd
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Order, Products  # Adjusted import statement
-import pickle
-
-# # Load the model and encoders
-# with open('./model/knn_model_features_base.pkl', 'rb') as model_file:
-#     loaded_model = pickle.load(model_file)
-
-# with open('./model/encoders_features_base.pkl', 'rb') as encoder_file:
-#     loaded_encoders = pickle.load(encoder_file)
-
-# # Define the features
-# features = ['gender', 'masterCategory', 'subCategory', 'articleType', 'season']
-
-@csrf_exempt
-def recommend_product(request, user_id):
-    if request.method == 'GET':
-        try:
-            # Fetch the last order for the user
-            feature = Products.objects.all().values_list('gender','mastercategory','subcategory','articletype','season', flat=True)
-            target =  list(Products.objects.all().values_list('id', flat=True))
-            new = pd.DataFrame([target])
-            print("Target---", new)
-            previous_order = Order.objects.filter(user=user_id).last()
-            product = previous_order.product
-            
-            # Prepare new data point
-            new_data = {
-                'gender': product.gender,
-                'masterCategory': product.mastercategory,
-                'subCategory': product.subcategory,
-                'articleType': product.articletype,
-                'season': product.season
-            }
-            
-            # Encode new data
-            new_df = pd.DataFrame([new_data])
-            print("New DF---", new_df)
-            for feature in features:
-                print("Data type----", type(new_df[feature]))
-                print("Data----", new_df[feature])
-                new_df[feature] = loaded_encoders[feature].transform(new_df[feature])
-            
-            # Make predictions
-            print("before prediction")
-            distances, indices = loaded_model.kneighbors(new_df, n_neighbors=3)
-            print("Prediction---", indices)
-            # Filter valid IDs
-            valid_ids = []
-            for i in range(len(indices[0])):
-                neighbor_index = indices[0][i]
-                neighbor_id = Products.objects.all()[neighbor_index].id  # Adjusted model name
-                if Products.objects.filter(id=neighbor_id).exists():
-                    valid_ids.append(neighbor_id)
-            
-            print("valid id's ", valid_ids)
-            return JsonResponse({"predicted_ids": valid_ids})
-            
-        
-        except Exception as e:
-            return JsonResponse({"error": str(e)})
-
-
-
-# views.py
-from django.shortcuts import render
-from django.http import JsonResponse
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.neighbors import NearestNeighbors
-import pickle
-
-# Define global variables for model and encoders
-knn_model = None
-encoders = None
-
-def train_model(request):
-    global knn_model, encoders
+@api_view(['GET'])
+def Recommend_product(request,user_id):
+    previous_orders = Order.objects.filter(user=user_id).order_by('-id')
+    user_history = UserHistory.objects.filter(user=user_id).order_by('-id')[:2]
+    products_ = []
+    for order in previous_orders:
+        new_data = { 
+        'gender': order.product.gender,
+        'masterCategory': order.product.mastercategory,
+        'subCategory': order.product.subcategory,
+        'articleType': order.product.articletype,
+        'season': order.product.season
+        }
+        products_.append(new_data)
     
-    # Load your dataset
-    # data_df = pd.read_csv('./Ecommerce_final.csv')
+    for user in user_history:
+        new_data = { 
+        'gender': user.product.gender,
+        'masterCategory': user.product.mastercategory,
+        'subCategory': user.product.subcategory,
+        'articleType': user.product.articletype,
+        'season': user.product.season
+        }
+        products_.append(new_data)
 
-    # Define features and target
-    products = Products.objects.all().values('gender', 'mastercategory', 'subcategory', 'articletype', 'season')
-    features_ = list(products)
-    features = ['gender', 'masterCategory', 'subCategory', 'articleType', 'season']
-    target =  list(Products.objects.all().values_list('id', flat=True))
-    # print("Target ----", target)
-    X = pd.DataFrame.from_records(features_)
-    y = pd.DataFrame(target,columns=['id'])
-    print("Columns---",y)
-    print("y Columns---",y.columns)
-    # print("feature---", X)
-    # print("Id ss-",y)
-    # Convert categorical variables into numerical values using LabelEncoder
-    encoders = {}
+    new_df = pd.DataFrame([new_data])
+    print("New Df---", new_df)
     for feature in features:
-        print("Data type----", type(feature))
-        encoders[feature] = LabelEncoder()
-        X[feature] = encoders[feature].fit_transform(X[feature])
+        new_df[feature] = loaded_encoders[feature].transform(new_df[feature])
 
-    # Train-Test Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    distances, indices = loaded_model.kneighbors(new_df, n_neighbors=3)
+    predicted_ids = product_predict.iloc[indices[0]]
+    print("Id's---", predicted_ids)
+    products = Products.objects.filter(id__in=predicted_ids)
+    print("Products----", products)
+    serializer = ProductReadSerializer(products, many=True)
+    print("Serializer data----", serializer.data)
+    return Response(serializer.data)
 
-    # Model Training (KNN)
-    knn_model = NearestNeighbors(n_neighbors=3)  # Get 3 nearest neighbors (IDs)
-    knn_model.fit(X_train, y_train)
 
-    # Save the trained KNN model
-    filename = 'knn_model_features_base.pkl'
-    pickle.dump(knn_model, open(filename, 'wb'))
-
-    # Save the LabelEncoders for future use
-    encoder_filename = 'encoders_features_base.pkl'
-    pickle.dump(encoders, open(encoder_filename, 'wb'))
-
-    return JsonResponse({'status': 'Model trained successfully.'})
